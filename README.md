3820c992b48e2a4d2c88d9a90c3957219aa2ee85
amadeus211 <malyuk2004.mail.ru@gmail.com>
2024-06-20 17:53:18 +0300

    titles and icons

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 react rsbuild weather app with interactive map
 
 
-npm i
+
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import ReactDOM from "react-dom/client";
 import * as React from "react";
 import { createRoot } from "react-dom/client";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
 import {
   createBrowserRouter,
   RouterProvider,
@@ -14,21 +16,43 @@ import WeatherPage from "./components/WeatherPage";
 import "./App.css";
 import Weather from "./components/Weather";
 import HomePage from "./components/HomePage";
+import WeatherIcon from "./images/weather.png"
+
+import Home from "./images/home.png";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
+    element: (
+      <>
+        <Helmet>
+          <title>Home Page</title>
+          <link rel="icon" type="image/x-icon" href={Home} />
+        </Helmet>
+        <HomePage />,
+      </>
+    ),
   },
   {
     path: "/weather",
-    element: <WeatherPage />,
+    element: (
+      <>
+        <Helmet>
+          <title>Weather</title>
+          <link rel="icon" type="image/x-icon" href={WeatherIcon} />
+        </Helmet>
+        <WeatherPage />,
+      </>
+    ),
   },
 ]);
 
 createRoot(document.getElementById("root")).render(
-  
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <HelmetProvider>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </HelmetProvider>
 );
+
+document.title = "23423";
